bc0931a8b10243ec9f088a93317515045be5ae97
Mariane <mariane.0306@outlook.com>
2024-03-05 15:03:19 -0300

    Alteração na porta da aplicação

--- bin/keys.js
+++ bin/keys.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     server: {
-        port: process.env.port || 3000
+        port: process.env.PORT || 3000
     },
     database: {
         connection: process.env.connection || 'mongodb+srv://barbosamariane538:jgSH78dXRjtIlruk@cluster0.tm3jzvo.mongodb.net/adoptme?retryWrites=true&w=majority&appName=Cluster0'
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./bin/index');
 const keys = require('./bin/keys');
 const connection = require('./src/middleware/connection');
 
-app.listen(keys.server.port, () => {
+app.listen(keys.server.PORT, () => {
     connection.connect();
     console.log('Estou funcionando');
 })
